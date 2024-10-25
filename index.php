cf1618ce6236d7f7999f5ff135f920b3cc5acaa2
ammyaroche <ammyaroche202006328@gmail.com>
2024-10-24 23:32:04 -0600

    2024-proyectos-evalucion-parcial-II

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Parcial 2</h1>
+    <br>
+    <table border="1">
+        <tr>
+            <td> Proyecto de software </td>
+        </tr>
+        <tr>
+            <td>Tipos de proyectos de software</td>
+        </tr>
+        <tr>
+            <tr>Ciclo de vida del software y sus fases</tr>
+        </tr>
+    </table>
+</body>
+</html>
